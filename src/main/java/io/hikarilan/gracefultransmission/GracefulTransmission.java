cf2f9c6083ea55ebec01031c68fe52d0be194b2b
HikariLan <hikarilan@minecraft.kim>
2023-07-12 16:26:11 +0800

    Initial commit

---
+++ src/main/java/io/hikarilan/gracefultransmission/GracefulTransmission.java
@@ -0,0 +1,16 @@
+package io.hikarilan.gracefultransmission;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GracefulTransmission extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
